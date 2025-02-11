5cfc85d79d56ff4a45c325e33ff9ff4d1f2fbd3b
maranix <ramanverma4183@gmail.com>
2025-02-11 13:45:04 +0530

    cleanup

--- constants/colors.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-    darkJungleGreen: "#242423", // Dark
-    greenWhite: "#E8EDDF", // Light
-    saffronMango: "#F5CB5C", // Accent Color
-    Nebula: "#CFDBD5", // Highlight Color
-};
